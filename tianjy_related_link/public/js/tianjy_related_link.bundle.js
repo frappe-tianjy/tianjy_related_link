ffc35546a834311826d2e2971034597b9827eddc
Fierflame <git@fierflame.com>
2023-12-26 17:28:27 +0800

    新增：自动带出关联字段功能增加对关联字段的支持

--- tianjy_related_link/public/js/tianjy_related_link.bundle.js
+++ tianjy_related_link/public/js/tianjy_related_link.bundle.js
@@ -1,3 +1,4 @@
 import './controls';
 import './formatters';
+import './frappe.model.DocTypeController.prototype.setup_fetch_from_fields';
 frappe.model.no_value_type.push('Tianjy Table Related MultiSelect');
